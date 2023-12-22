fa95dd7346aef13703800d8ec9645cbbde235a50
matei <mateipufosul@gmail.com>
2023-12-22 22:23:38 +0200

    Initial commit

---
+++ app/src/main/java/com/rodo/estiprost/EstiProstKotlin.kt
@@ -0,0 +1,11 @@
+package com.rodo.estiprost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EstiProstKotlin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin)
+    }
+}
